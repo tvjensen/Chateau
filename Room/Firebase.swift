6a38028bbf818beef736afb6aead5ab368f2e356
csmithy <csmith95@stanford.edu>
2018-05-03 14:14:05 -0700

    working on map

--- Room/Firebase.swift
+++ Room/Firebase.swift
@@ -12,8 +12,16 @@ import FirebaseDatabase
 class Firebase {
     
     // use this to interact with db
-    let ref = Database.database().reference()
+    private static let ref = Database.database().reference()
+    private static let roomsRef = ref.child("rooms")
     
-    
+    // start observing rooms so that explore page can display
+    public static func startObservingRooms() {
+//        callback: ((Models.Room) -> (Void)
+//        roomsRef.observe(DataEventType.childAdded) { (snapshot) in
+//            guard let room = Models.Room(snapshot.value) else { callback(nil); return; }
+//            callback(room)
+//        }
+    }
     
 }
