ed4db1a0834f9af1cb4bc2b418eefd4c26617427
Rick Duenas <rduenas@stanford.edu>
2018-05-16 17:02:26 -0700

    Fleshes out table cells by creating table view cell superclass (still in progress)

---
+++ Room/MyRoomPreviewTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  MyRoomPreviewTableViewCell.swift
+//  Room
+//
+//  Created by Rick Duenas on 5/16/18.
+//  Copyright © 2018 csmith. All rights reserved.
+//
+
+import UIKit
+
+class MyRoomPreviewTableViewCell: UITableViewCell {
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var nMemLabel: UILabel!
+    @IBOutlet weak var lastActivityLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
